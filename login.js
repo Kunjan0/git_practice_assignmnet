d81d12ba915012d12142f013a7d0c4696963d5b2
Kunjan Ballawar <kballawar0@gmail.com>
2022-09-13 23:26:45 +0530

    prime & Palindrome

---
+++ login.js
@@ -0,0 +1,26 @@
+//Prime Problem
+
+let num= 15;
+
+ let count = 0;
+
+for(i=0; i<=num; i++){
+  
+   if(num%1==0){
+  
+  count++;
+
+  if(count==2){
+console.log("Prime Number");
+
+
+
+//Palindrome problem
+
+let str= "Himanshu";
+let bag= "";
+
+for(i=str-1; i>=0; i--){
+
+  bag= bag + str[i];
+console.log(bag);
